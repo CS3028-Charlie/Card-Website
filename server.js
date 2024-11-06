41aadc2872f4ebb5234b1d56b7a8d6dc2fa68ffb
Fraser McCarter <114738535+spxllz@users.noreply.github.com>
2024-11-06 16:33:38 +0000

    js adjustment

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const app = express();
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public'))); // Adjust folder if needed
 
+// Route to serve index.html or any other pages
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'shop.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
 // Handle all routes and send back shop.html (for SPA)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'shop.html'));
