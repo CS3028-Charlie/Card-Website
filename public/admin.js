b8f166ba7b3abd00c2bb0c552b733ebd2e3a7fd5
K-Jacob8 <k.jacob.22@abdn.ac.uk>
2024-11-11 18:32:12 +0000

    update api url

--- public/admin.js
+++ public/admin.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:3000"
+//const API_URL = "http://localhost:3000"
+const API_URL = "https://charlie-card-backend-fbbe5a6118ba.herokuapp.com"
 
 // image upload validation
 document.getElementById("uploadForm").addEventListener("submit", function (event) {
